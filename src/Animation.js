e003c4258718cc35364dbeece5b66b92263a4f83
Rodrigo Garcia <dev.raxielh@gmail.com>
2024-08-28 19:08:07 -0500

    init

---
+++ src/Animation.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const AnimationSwitcher = () => {
+    return (
+        <div className="max-w-3xl mx-auto">
+        </div>
+    );
+};
+
+export default AnimationSwitcher;
